d55d6e45adc0299956e39a4078738178d6c2bb21
Travis Whitten <traviswhitten@Traviss-Mac-mini.local>
2016-10-07 23:39:43 -0400

    Completed setting up firebase

---
+++ MyMoves/SignInVC.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MyMoves
+//
+//  Created by Travis Whitten on 10/7/16.
+//  Copyright © 2016 Travis Whitten. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
